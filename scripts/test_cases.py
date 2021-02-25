1331ccbeba587015fc74fdd639fb598c02280c12
sohaib ajmal <sohaib.ajmal1993@gmail.com>
2021-02-25 17:26:46 +0500

    update

--- scripts/test_cases.py
+++ scripts/test_cases.py
@@ -47,13 +47,13 @@ def setup_environment(keypair_name, security_group_name, token):
     create_keypair(settings["key_name"],token)
     create_security_group(settings["security_group"],token)
     add_icmp_rule(settings["security_group"],token)
-    add_ssh_rule(settings["security_group"],token)
+    add_ssh_rule(setstings["security_group"],token)
     create_keypair(settings["key_name"],token)
     image_verify(settings["image"],token)
     flavor_verify(settings["flavor"],token)
 
-def numa_test_cases(nova_ep, neutron_ep, glance_ep, token, settings):
-    numa_test_case_3(nova_ep, neutron_ep, glance_ep, token, settings)
+def numa_test_cases(nova_ep, neutron_ep, glance_ep, image_ep, token, settings):
+    numa_test_case_3(nova_ep, neutron_ep, glance_ep, image_ep, token, settings)
 
 def main():
    
@@ -88,23 +88,40 @@ def main():
     neutron_ep= settings["dashboard_ip"]+":9696"
     cinder_ep= settings["dashboard_ip"]+":8776"
     nova_ep= settings["dashboard_ip"]+":8774"
-    glance_ep=""
-    #neutron_ep=
+    image_ep= settings["dashboard_ip"]+":9292"
+    glance_ep= ""
 
     #Get Authentication token
     logging.info("auhenticating user")
     token= get_authentication_token(keystone_ep, settings["username"], settings["password"]  )
-    print("Token Received") 
+    print("Token Received:" +token) 
     logging.info("Glance endpoint {}".format(glance_ep))   
     logging.info("Neutron endpoint {}".format(neutron_ep))  
     logging.info("Cinder endpoint {}".format(cinder_ep))  
     logging.info("Nova endpoint {}".format(nova_ep))  
 
+    #Get Undercloud Authentication Token
+    undercloud_token= get_authentication_token(settings["undercloud_keystone_ip"], settings["undercloud_username"], settings["undercloud_password"])
+    print("undercloud token: {}".format(undercloud_token))
+    print("Get VMS")
+    
+    servers= receive_all_server("http://192.168.120.10:8774", undercloud_token)
+    server_ips=[]
+    for server in servers["servers"]:
+        list= server["name"].split('-')
+        server_ip= {list[-2]+list[-1], server["addresses"]["ctlplane"][0]["addr"]}
+        server_ips.append(server_ip)
+    print("IPSSS")
+    print(server_ips)
+    print(server_ips["compute0"])
+
+
+
     
 
     #Run Test Cases
-    if arguments.feature == "numa":
-        numa_test_cases(nova_ep, neutron_ep, glance_ep, token, settings) 
+#    if arguments.feature == "numa":
+ #       numa_test_cases(nova_ep, neutron_ep, glance_ep, image_ep,token, settings) 
 
 if __name__ == "__main__":
     main()
